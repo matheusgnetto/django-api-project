e285d3bb77d8495c31f2dbe41a3d9c3e580ae775
matheusgnetto <matheusgomesnetto@gmail.com>
2022-09-27 22:50:34 -0300

    correcao na producao

--- api/settings/producao.py
+++ api/settings/producao.py
@@ -5,9 +5,9 @@ env = environ.Env()
 
 DEBUG = env.bool("DEBUG", False)
 
-SECRET_KEY = env("SECRET KEY")
+SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = env.list("ALLOWEB HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 DATABASES = {
     "default": env.db()
